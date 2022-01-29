c888f9adebe4b40a764026f50987800f28f601d2
sharon892 <cherosharon774@gmail.com>
2022-01-29 05:47:49 -0800

    updates

---
+++ introduction.php
@@ -0,0 +1,9 @@
+<?php
+echo "My first php class";
+echo "<br>";
+echo "I love php";
+echo "<br>";
+echo "Today's class was amazing";
+
+
+?>
---
+++ variables.php
@@ -0,0 +1,33 @@
+<?php
+$jina="Sharon Bee";
+//Kindly variables are case sensitive
+$JINA="Dorine Chege";
+echo "My name is $jina";
+echo "<br>";
+echo "My classmate is $JINA";
+echo "<br>";
+$nambari=90;
+$nambari1=70;
+echo $nambari+$nambari1;
+echo "<br>";
+$y=90;
+$x=20;
+$p=$y*$x;
+echo"The product is $p";
+echo "<br>";
+$y=90;
+$x=20;
+$p=$y-$x;
+echo "The difference is $p";
+echo "<br>";
+$y=90;
+$x=20;
+$p=$y/$x;
+echo "The dividend is $p";
+echo "<br>";
+$y=90;
+$x=20;
+$p=$y%$x;
+echo"The modulus is $p";
+
+
